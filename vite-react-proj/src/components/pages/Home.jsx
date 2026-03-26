e91624227c2ff91683dbab8dd9704f1959c8ef36
Shoaib Mushtaq Bhat <114304104+shuaibkashmiri@users.noreply.github.com>
2026-03-26 14:54:15 +0530

    useState

--- vite-react-proj/src/components/pages/Home.jsx
+++ vite-react-proj/src/components/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="main">
-      <h1>Home</h1>
+      <button onClick={increaseCount}>Increase</button>
+      {count > 0 ? <h1 className="red">{count}</h1> : <h1>{count}</h1>}
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   );
 };
