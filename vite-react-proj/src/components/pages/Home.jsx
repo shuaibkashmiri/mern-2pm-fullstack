1d8039ed82c2aed4707238ee6b17bf244e41e2de
Shoaib Mushtaq Bhat <114304104+shuaibkashmiri@users.noreply.github.com>
2026-03-30 15:04:25 +0530

    fix

--- vite-react-proj/src/components/pages/Home.jsx
+++ vite-react-proj/src/components/pages/Home.jsx
@@ -1,44 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Imagecard from "../sharedCompnents/Imagecard";
+import axios from "axios";
 
 const Home = () => {
-  const users = [
-    {
-      id: 1,
-      name: "Alex Johnson",
-      role: "Senior Developer",
-      image:
-        "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=400&auto=format&fit=crop",
-    },
-    {
-      id: 2,
-      name: "Sarah Chen",
-      role: "Product Designer",
-      image:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=400&auto=format&fit=crop",
-    },
-    {
-      id: 3,
-      name: "Marcus Rodriguez",
-      role: "Marketing Manager",
-      image:
-        "https://images.unsplash.com/photo-1500648767793-00020114a33e?q=80&w=400&auto=format&fit=crop",
-    },
-    {
-      id: 4,
-      name: "Aisha Khan",
-      role: "UX Researcher",
-      image:
-        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=400&auto=format&fit=crop",
-    },
-    {
-      id: 5,
-      name: "Liam O'Connor",
-      role: "DevOps Engineer",
-      image:
-        "https://images.unsplash.com/photo-1539571696357-5a69c1786d4b?q=80&w=400&auto=format&fit=crop",
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(res.data);
+      console.log(users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    // console.log(getData());
+  }, []);
 
   return (
     <div className="main">
@@ -46,8 +26,11 @@ const Home = () => {
 
       ))} */}
       <div className="box">
-        {users.map((user) => (
+        {/* {users.map((user) => (
           <Imagecard name={user.name} image={user.image} role={user.role} />
+        ))} */}
+        {users.map((user) => (
+          <p>{user.name}</p>
         ))}
       </div>
     </div>
